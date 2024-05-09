b2c63dc7542ee1872f0b94fa8e2d3af7caa3ad5d
EslamFayez123 <eng.eslam.fayez@gmail.com>
2024-05-09 20:39:31 +0300

    update with comment for illustrating the code

--- src/main/scala/rule.scala
+++ src/main/scala/rule.scala
@@ -21,7 +21,12 @@ object rule extends App {
 
   val f: File = new File("src/main/scala/RESULT.csv")
   val writer = new PrintWriter(new FileOutputStream(f,false))
-
+  /**
+   * Logs the execution of a rule along with a message indicating success or failure.
+   *
+   * @param logMessage The message to be logged.
+   * @param success    A boolean indicating whether the execution was successful or not.
+   */
   def logExecution(logMessage: => String, success: Boolean): Unit = {
     if (success) {
         logger.info(logMessage)
@@ -29,7 +34,13 @@ object rule extends App {
       logger.debug(logMessage)
     }
   }
-
+  /**
+   * Calculates the number of days between two given dates and applies rule1 logic.
+   *
+   * @param startdate The start date in "yyyy-MM-dd" format.
+   * @param enddate   The end date in "yyyy-MM-dd" format.
+   * @return          The result according to rule1 logic.
+   */
       def rule1(startdate: String, enddate: String): Int = {
         try {
           val format = new SimpleDateFormat("yyyy-MM-dd")
@@ -50,7 +61,12 @@ object rule extends App {
             0
         }
       }
-
+  /**
+   * Applies rule2 logic based on the given product name and returns the discount.
+   *
+   * @param productname The name of the product.
+   * @return            The discount according to rule2 logic.
+   */
       def rule2(productname: String): Int = {
         try {
           val cheese_discount = 10
@@ -71,7 +87,13 @@ object rule extends App {
             0
         }
       }
-
+  /**
+   * Applies rule3 logic based on the given date string and target date, returning the result.
+   *
+   * @param dateString The date string to compare.
+   * @param target     The target date string to compare against.
+   * @return           The result according to rule3 logic.
+   */
       def rule3(dateString: String, target: String): Int = {
         try {
           val format = new SimpleDateFormat("yyyy-MM-dd")
@@ -91,7 +113,12 @@ object rule extends App {
             0
         }
       }
-
+  /**
+   * Applies rule4 logic based on the given units string and returns the result.
+   *
+   * @param unitsString The string representing units.
+   * @return            The result according to rule4 logic.
+   */
       def rule4(unitsString: String): Int = {
         try {
           val unitsInt = unitsString.toInt
@@ -114,7 +141,13 @@ object rule extends App {
             0
         }
       }
-
+  /**
+   * Applies rule5 logic based on the given quantity and payment method, returning the result.
+   *
+   * @param quantity       The quantity of items.
+   * @param payment_method The method of payment.
+   * @return               The result according to rule5 logic.
+   */
       def rule5(quantity: String, payment_method: String): Int = {
         try {
           if (payment_method.toLowerCase.trim == "app") {
@@ -131,6 +164,12 @@ object rule extends App {
             0
         }
       }
+  /**
+   * Applies rule6 logic based on the given payment method and returns the result.
+   *
+   * @param payment The method of payment.
+   * @return        The result according to rule6 logic.
+   */
         def rule6(payment:String):Int = {
           try {
             if (payment.toLowerCase.trim == "visa") {
@@ -151,7 +190,12 @@ object rule extends App {
 
 
 
-
+  /**
+   * Calculates the average discount based on a list of orders.
+   *
+   * @param ordersList The list of orders.
+   * @return           The average discount calculated from applying various rules.
+   */
 
   def avg_discount(ordersList: List[String]): Float = {
     try{
@@ -173,21 +217,28 @@ object rule extends App {
         0
     }}
 
-
+  /**
+   * Applies discount to the orders list based on the average discount.
+   *
+   * @param orderslist    The list of orders.
+   * @param avg_discount  The function to calculate average discount.
+   * @return              A list containing the discount and the final price after applying the discount.
+   */
     def applydiscount(orderslist:List[String] ,avg_discount:List[String] => Float): List[Float] = {
     val discount = avg_discount(orderslist)
     val price = orderslist(4).toFloat * orderslist(3).toFloat
     val final_price = (price * (100-discount))/100
     List(discount,final_price)
   }
-  // Process each line
+  /// Process each line
   val finalList = lines.map { line =>
     val fields = line.split(",").toList
     applydiscount(fields, avg_discount)
   }
+  //print header
   writer.println("discount,final price")
 
-  // Write final list to CSV file
+  /// Write final list to CSV file
   finalList.foreach { x =>
     val line = x.mkString(",")
     writer.println(line)
@@ -199,13 +250,26 @@ object rule extends App {
   val username = "hr"
   val password = "123"
   val connection = connectToDatabase(url, username, password)
-
+  /**
+   * Establishes a connection to the database.
+   *
+   * @param url      The database URL.
+   * @param username The username for database authentication.
+   * @param password The password for database authentication.
+   * @return         A Connection object representing the database connection.
+   */
 
   def connectToDatabase(url: String, username: String, password: String): Connection = {
     Class.forName("oracle.jdbc.driver.OracleDriver")
     DriverManager.getConnection(url, username, password)
   }
-
+  /**
+   * Inserts data into the database.
+   *
+   * @param connection The Connection object representing the database connection.
+   * @param discount   The discount value to be inserted.
+   * @param finalPrice The final price value to be inserted.
+   */
 
   def insertData(connection: Connection, discount: Float, finalPrice: Float): Unit = {
     val query = "INSERT INTO discount (discount, finalprice) VALUES (?, ?)"
